421880dc58caa8abccc4d53339aa0a678bf357a9
lfuentesg10-byte <lfuentesg10@miumg.edu.gt>
2026-08-28 21:47:20 -0600

    Add files via upload

---
+++ SistemaReservacionesHotel/src/modelo/Habitual.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Habitual {
+    private double descuento;
+
+    public Habitual(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+}
