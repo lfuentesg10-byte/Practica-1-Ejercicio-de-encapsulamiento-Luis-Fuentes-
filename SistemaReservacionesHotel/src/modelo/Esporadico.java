f6e0c579c4cd92549082d00ddfc9b2d8d7467696
lfuentesg10-byte <lfuentesg10@miumg.edu.gt>
2026-08-28 22:00:54 -0600

    Add files via upload

---
+++ SistemaReservacionesHotel/src/modelo/Esporadico.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Esporadico {
+    private String infoOfertas;
+
+    public Esporadico(String infoOfertas) {
+        this.infoOfertas = infoOfertas;
+    }
+
+    public String getInfoOfertas() {
+        return infoOfertas;
+    }
+
+    public void setInfoOfertas(String infoOfertas) {
+        this.infoOfertas = infoOfertas;
+    }
+}
